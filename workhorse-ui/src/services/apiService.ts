cebbb88c85b969df87505e06f463a0ba126e4eea
LiamWIll <willywillsken@gmail.com>
2025-09-30 12:35:11 +0400

    pt, update .env.local file

--- workhorse-ui/src/services/apiService.ts
+++ workhorse-ui/src/services/apiService.ts
@@ -2,7 +2,7 @@ import { CreateJobRequest, Job, JobStatus, WorkerStatus } from "@/types/dashboar
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
     withCredentials: true
 })
 
